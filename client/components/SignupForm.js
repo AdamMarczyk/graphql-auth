e53741692688b3c2f85094b506907047e25b12f0
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-20 08:48:49 +0200

    add signup route

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -4,11 +4,17 @@ import AuthForm from './AuthForm';
 import mutation from '../mutations/Signup';
 
 class SignupForm extends React.Component {
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password }
+    });
+  }
+
   render() {
     return (
       <div>
         <h3>Sign Up</h3>
-        <AuthForm />
+        <AuthForm errors={[]} onSubmit={this.onSubmit.bind(this)} />
       </div>
     );
   }
